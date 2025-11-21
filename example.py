1c7cce414136e0d1a254334242d82cac6ae0f7df
QnK1 <kacperro03@gmail.com>
2025-11-21 13:24:53 +0100

    fixed default motor control blocking spherical joint movement, various other creature and simulation fixes

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ from simulation.simulation import Simulation
 from evolution.neural_network import NeuralNetwork
 from evolution.fitness import XDistanceFitness
 from core.types import GeneticAlgorithmParams, FullJointStateGetter, TimeOnlyRunConditions
-from core.orchestrate import run_population
+from core.orchestrate import run_population, _run_individual
 from evolution.population import generate_random_individual
 from evolution.genome_visualization import visualize_network
 
@@ -17,27 +17,40 @@ from evolution.genome_visualization import visualize_network
 #  a temporary test file
 #
 
-if __name__ == "__main__":
-    start_time = time.perf_counter()
+# if __name__ == "__main__":
+#     start_time = time.perf_counter()
 
-    pop = [generate_random_individual(16, 2, 2) for _ in range(1000)]
+#     pop = [generate_random_individual(16, 2, 2) for _ in range(1000)]
 
-    params = GeneticAlgorithmParams()
-    params.creature_path = Path("assets/creatures/2-spherical_2-revolute_two-arm-biped.urdf")
-    params.fitness = XDistanceFitness()
-    params.indiv_output_scale = 1000
-    params.n_processes = None
-    params.state_getter = FullJointStateGetter()
-    params.run_conditions = TimeOnlyRunConditions(10)
-    results = run_population(pop, params)
+#     params = GeneticAlgorithmParams()
+#     params.creature_path = Path("assets/creatures/2-spherical_2-revolute_two-arm-biped.urdf")
+#     params.fitness = XDistanceFitness()
+#     params.indiv_output_scale = 1000
+#     params.n_processes = None
+#     params.state_getter = FullJointStateGetter()
+#     params.run_conditions = TimeOnlyRunConditions(10)
+#     results = run_population(pop, params)
 
 
-    for r in results:
-        print(r)
+#     for r in results:
+#         print(r)
     
-    print(len(results))
+#     print(len(results))
 
-    end_time = time.perf_counter()
+#     end_time = time.perf_counter()
 
-    execution_time = end_time - start_time
-    print(f"The code executed in {execution_time:.6f} seconds")
+#     execution_time = end_time - start_time
+#     print(f"The code executed in {execution_time:.6f} seconds")
+
+params = GeneticAlgorithmParams()
+params.creature_path = Path("assets/creatures/1-spherical_hopper.urdf")
+params.fitness = XDistanceFitness()
+params.indiv_output_scale = 10
+params.n_processes = None
+params.state_getter = FullJointStateGetter()
+params.run_conditions = TimeOnlyRunConditions(10)
+
+sim = Simulation(p.GUI, Path("assets/creatures/1-spherical_hopper.urdf"))
+
+indiv = generate_random_individual(6, 0, 1)
+_run_individual(indiv, sim, params)
